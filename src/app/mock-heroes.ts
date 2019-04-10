b4b01f9d1d6c0fdd7b28c86384e6a0d2d0ad7546
jinyung <jinyungisnot@gmail.com>
2019-04-10 16:21:27 +0800

    Hero-Detail Component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id : 11, name: 'Mr. Nice'},
+  { id : 12, name: 'Narco'},
+  { id : 13, name: 'Mr. White'},
+  { id : 14, name: 'PacMan'},
+  { id : 15, name: 'Orange Guy'},
+  { id : 16, name: 'Rectangle Man'},
+  { id : 17, name: 'Ms. Oval'},
+  { id : 18, name: 'Josh'},
+  { id : 19, name: 'Jeremy'},
+  { id : 20, name: 'Sam'}
+]
